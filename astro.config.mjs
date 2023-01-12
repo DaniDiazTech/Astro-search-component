2c58be63ae543217fa5e79ed20ae2338d6d13cf9
Daniel Diaz <danieldiazgonza52@gmail.com>
2023-01-12 18:54:23 -0500

    Added styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,9 @@ import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 
 // https://astro.build/config
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), react()]
+  integrations: [mdx(), react(), tailwind()]
 });
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -28,32 +28,67 @@ function Search({ searchList }) {
   }
 
   return (
-	<>
-		<label>Search</label>
-		<input type="text" value={query} onChange={handleOnSearch} placeholder="Search posts"/>
-		{
-			query.length > 1 && (
-				<p>
-					Found {posts.length} {posts.length === 1 ? 'result' : 'results'} for '{query}'
-				</p>
-			)
-		}
-		<ul>
-			{
-				posts && posts.map(post => 
-					<li>
-						<a href={`/${post.frontmatter.slug}`}>
-						
-							{post.frontmatter.title}
-						</a>
-						{post.frontmatter.description}
-						
-					</li>	
-				)
-			}
-		</ul>
-	</>
-  )
+    <div>
+      <label
+        htmlFor="search"
+        className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
+      >
+        Search
+      </label>
+      <div className="relative">
+        <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="icon icon-tabler icon-tabler-search"
+            width={24}
+            height={24}
+            viewBox="0 0 24 24"
+            strokeWidth="2"
+            stroke="currentColor"
+            fill="none"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          >
+            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+            <circle cx={10} cy={10} r={7}></circle>
+            <line x1={21} y1={21} x2={15} y2={15}></line>
+          </svg>
+        </div>
+        <input
+          type="text"
+          id="search"
+          value={query}
+          onChange={handleOnSearch}
+          className="block w-full p-4 pl-10 text-sm 
+                                text-gray-900 
+                               border border-gray-300
+                               rounded-lg bg-gray-50
+
+                               focus:outline-none
+                               focus:ring-blue-500
+                               focus:border-blue-500"
+          placeholder="Search for anything..."
+        />
+      </div>
+
+      {query.length > 1 && (
+        <div className="my-4">
+          Found {posts.length} {posts.length === 1 ? "result" : "results"} for '
+          {query}'
+        </div>
+      )}
+
+      <ul className="list-none">
+        {posts &&
+          posts.map((post) => (
+            <li className="py-2">
+              <a className="text-lg text-blue-700 hover:text-blue-900 hover:underline underline-offset-2" href={`/${post.frontmatter.slug}`}>{post.frontmatter.title}</a>
+              <p className="text-sm text-gray-800">{post.frontmatter.description}</p>
+            </li>
+          ))}
+      </ul>
+    </div>
+  );
 }
 
 export default Search;
